0488cbee90f3af72bad530430e59e8a807287d7e
raghu6289 <99999872+raghu6289@users.noreply.github.com>
2024-11-28 16:03:07 +0530

    Update connect.js

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    // const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.createConnection(process.env.MONGO_URI);
     console.log(`mongodb connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error.message);
